29e2dfd5dfc776aacbd2b08a8bc1e3a836674f4c
nathanloo9927 <ngloo@NN123-HA-04.HS.sd203.org>
2015-11-23 15:09:42 -0600

    OOPS!

--- Chapter 7 Practice/PartialArray.java
+++ Chapter 7 Practice/PartialArray.java
@@ -19,8 +19,48 @@ public class PartialArray
         }
         this.currentSize--;
     }
-    /*public static void main(String[] args)
+    public void insert(int pos, int newValue)
     {
-        
-    }*/
+        if (this.currentSize == this.values.length)
+        {
+            this.grow();
+        }
+        for (int i = currentSize; i > pos; i--)
+        {
+            this.values[i] = this.values[i-1];
+        }
+        this.values[pos] = newValue;
+        currentSize++;
+    }
+    public void swap(int posA, int posB)
+    {
+       int temp = this.values[posA];
+       this.values[posA] = this.values[posB];
+       this.values[posB] = temp;
+    }
+    private void grow()
+    {
+        int[] newValues = new int[this.currentSize * 2];
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            newValues[i] = this.values[i];
+        }
+        this.values = newValues;
+    }
+    public static void main(String[] args)
+    {
+        // DO NOT DARE RUN THIS!!!
+        PartialArray part = new PartialArray();
+        int d = 1;
+        while (d != 0)
+        {
+            System.out.println("Menu:");
+            System.out.println("1-remove an element");
+            System.out.println("2-insert an element");
+            System.out.println("3-swap 2 elements");
+            System.out.println("0-quit");
+            
+            
+        }
+    }
 }
