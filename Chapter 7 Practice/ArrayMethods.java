44c2285ea3ccefee88131bc7283f58e8d571e2c4
nathanloo9927 <ngloo@NN123-HA-04.HS.sd203.org>
2015-11-30 15:10:02 -0600

    "I can do all things through christ who gives me strength" - Matthew West, Strong Enough

--- Chapter 7 Practice/ArrayMethods.java
+++ Chapter 7 Practice/ArrayMethods.java
@@ -6,25 +6,30 @@ public class ArrayMethods
     public ArrayMethods(int[] initialValues)
     {
         this.values = initialValues;
-        for (this.currentSize = 0; this.currentSize < initialValues.length; this.currentSize++)
-        {
-            this.values[this.currentSize] = this.currentSize * this.currentSize;
-        }
+        this.currentSize = this.values.length;
     }
     public void swapFirstAndLast()
     {
         int temp = this.values[0];
         this.values[0] = this.values[this.values.length - 1];
         this.values[this.values.length - 1] = temp;
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            System.out.print(this.values[i] + " ");
+        }
     }
     public void shiftRight()
     {
         int temp = this.values[this.values.length - 1];
-        for (int i = this.values.length - 2; i >= 1 ; i--)
+        for (int i = this.values.length - 1; i >= 1 ; i--)
         {
             this.values[i] = this.values[i-1];
         }
         this.values[0] = temp;
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            System.out.print(this.values[i] + " ");
+        }
     }
     public void replaceEvens()
     {
@@ -34,6 +39,10 @@ public class ArrayMethods
             {
                 this.values[i] = 0;
             }
+        }
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            System.out.print(this.values[i] + " ");
         }
     }
     public void replaceWithLarger()
@@ -67,16 +76,59 @@ public class ArrayMethods
             }
             this.currentSize--;
         }
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            System.out.print(this.values[i] + " ");
+        }
     }
     public void moveEvensToFront()
     {
-        int[] temparray = new int[this.values.length];
+        int[] temparray = new int[this.currentSize];
         for (int i = 0; i < this.values.length; i++)
         {
             if (this.values[i] % 2 == 0)
             {
-                temparray[i] = values[i];
+                //temparray[i] = this.values[i];
             }
         }
+        for (int i = 0; i < this.values.length; i++)
+        {
+            if (this.values[i] % 2 != 0)
+            {
+                //temparray[i] = this.values[i];
+            }
+        }
+        for (int i = 0; i < this.currentSize; i++)
+        {
+            System.out.print(temparray[i] + " ");
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] arrayA = {0,1,4,9,16,25};
+        int[] arrayB = {0,1,4,9,16,25};
+        int[] arrayC = {0,1,4,9,16,25};
+        int[] arrayD = {0,1,4,9,16,25};
+        int[] arrayE = {0,1,4,9,16,25};
+        ArrayMethods array1 = new ArrayMethods(arrayA);
+        ArrayMethods array2 = new ArrayMethods(arrayB);
+        ArrayMethods array3 = new ArrayMethods(arrayC);
+        ArrayMethods array4 = new ArrayMethods(arrayD);
+        ArrayMethods array5 = new ArrayMethods(arrayE);
+        System.out.println("The results after: ");
+        System.out.print("Reversing switch first and last: ");
+        array1.swapFirstAndLast();
+        System.out.println();
+        System.out.print("Shifting all elements except the last one right. put the last element first: ");
+        array2.shiftRight();
+        System.out.println();
+        System.out.print("Replacing all even elements with 0: ");
+        array3.replaceEvens();
+        System.out.println();
+        System.out.print("Removing the middle element: ");
+        array4.removeMiddle();
+        System.out.println();
+        System.out.print("Moving the evens to the front: ");
+        array5.moveEvensToFront();
     }
 }
--- Chapter 7 Practice/ArrayOperations.java
+++ Chapter 7 Practice/ArrayOperations.java
@@ -4,13 +4,13 @@ public class ArrayOperations
     public static void main(String[] args)
     {
         int[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100};
-        System.out.println(x.length);
+        System.out.println("Length: " + x.length);
         System.out.println();
-        System.out.println(x[0]);
+        System.out.println("First: "+ x[0]);
         System.out.println();
-        System.out.println(x[8]);
+        System.out.println("Last: " + x[8]);
         System.out.println();
-        System.out.println(x[x.length - 1]);
+        System.out.println("x[x.length - 1]: " + x[x.length - 1]);
         System.out.println();
         for (int i = 0; i < x.length; i++)
         {
